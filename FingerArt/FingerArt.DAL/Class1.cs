2ab338b695a6e365ce500b483b1e685bc9f79756
maydkw <1403603496@qq.com>
2018-03-19 11:06:57 +0800

    试试

---
+++ FingerArt/FingerArt.DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerArt.DAL
+{
+    public class Class1
+    {
+    }
+}
