98de819682ad48fcbfd7aaf10000b84f447ed6e9
wildlemon <wt19952014@outlook.com>
2018-03-19 10:56:46 +0800

    测试与练习

---
+++ FingerArt/FingerArt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FingerArt.Startup))]
+namespace FingerArt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
